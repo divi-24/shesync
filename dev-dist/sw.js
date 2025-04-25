22dedf7c273b186a4403fc4c427c8ddf7e43a765
Deepak Gupta <YOUR_EMAIL>
2025-04-25 23:50:46 +0530

    updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.33m40gi5lt"
+    "revision": "0.mf2jehp57a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
